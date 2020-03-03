82a70ffbe43b476d233b8e2dfcc4b92afe13d2d7
yuanye <707257485@qq.com>
2020-03-03 21:36:37 +0800

    add files about pipe

---
+++ second.c
@@ -0,0 +1,15 @@
+#include "unistd.h"
+#include "stdio.h"
+#include "sys/types.h"
+#include "stdlib.h"
+int main()
+{
+    int i;
+	for (i = 0; i < 5; ++i)
+	{
+		printf("this is second process\n");
+		usleep(100);
+	}
+	while(1);
+	return 0;
+}
